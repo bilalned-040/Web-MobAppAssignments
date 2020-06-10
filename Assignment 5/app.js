52ddb2efaa5651bbb521e4026d235d58037a7a1b
Muhammad Bilal Hanif <bilalhanifhalai@gmail.com>
2020-06-10 17:41:30 +0500

    upto assignment 5

---
+++ Assignment 5/app.js
@@ -0,0 +1,111 @@
+//                                                  CHAPTER 1:
+//TASK 1:
+// alert("Welcome to my website");
+//TASK 2:
+// alert("Error! Please enter a valid Password.");
+//TASK 3:
+// alert("Welcome to JS Land\nHapping Coding!");
+//TASK 4:
+// alert("Welcome to JS Land..");
+// alert("Happing Coding");
+//TASK 5:
+// alert("Hello... I can run JS through my web browser's console");
+
+
+
+//                                                  CHAPTER 2:
+//TASK 1:
+// var  username;
+//TASK 2:
+// var myName = "Bilal Hanif";
+//TASK 3:
+// var message;
+// message = "Hello World";
+// alert(message);
+//TASK 4:
+// var name="ibrahim";
+// var age=20;
+// var course="Certified Mobile Application Development";
+// alert(name);
+// alert(age+" years old");
+// alert(course);
+//TASK 5:
+// var str="PIZZA";
+// alert(str+"\n"+str.slice(0,4)+"\n"+str.slice(0,3)+"\n"+str.slice(0,2)+"\n"+str.slice(0,1));
+//TASK 6:
+// var email="bilalhanifhalai@gmail.com";
+// alert("My Email address is "+email);
+//TASK 7:
+// var book="A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book "+book);
+//TASK 8:
+// document.write("Yah! I can writeHTML content through JavaScript")
+//TASK 9:
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+// document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"); 
+
+
+//                                                  CHAPTER 3:
+//TASK 1:
+// var age = 20;
+// alert("I am "+age+" years old");
+//TASK 2:
+// var count = 10;
+// alert("You have visited this site "+count+" times");
+//TASK 3:
+// var birthYear=1999;
+// document.write("My birth year is "+birthYear+"<br>Data type of my declared variable is "+typeof(birthYear));
+//TASK 4:
+// var visitorName = "John Doe";
+// var productTitle= "T-shirt";
+// var quantity = 5;
+// document.write(visitorName.bold()+" ordered "+quantity.toString().bold()+" "+productTitle.bold()+"(s) on XYZ Clothing Store");
+
+
+
+
+//                                                  CHAPTER 4:
+//TASK 1:
+// var a,b,c;
+//TASK 2:
+// var $car,_students,myName,position1st,first_obj;
+// var const, 1st, @address, #num, a&b ;
+//TASK 3:
+// document.write("<h3>Rules for naming JS variables</h3><br>");
+// document.write("Variable names can only contain numbers, $ and _. For example: $my_1stVariable<br>");
+// document.write("Variables must begin with a letter, $ or _. For example $name, _name or name<br>");
+// document.write("Variable names are case sensitive<br>");
+// document.write("Variable names should not be JS keywords");
+
+
+
+//                                                  CHAPTER 5:
+//TASK 1:
+// var num1=parseInt(prompt("Enter 1st number:"));
+// var num2=parseInt(prompt("Enter 2nd number:"));
+// var sum=num1+num2;
+// document.write("Sum of "+num1+" and "+num2+" is "+sum);
+//TASK 2:
+// var sub=num1-num2;
+// document.write("Subtraction of "+num1+" and "+num2+" is "+sub);
+// var mul=num1*num2;
+// document.write("Multiplication of "+num1+" and "+num2+" is "+mul);
+// var div=num1/num2;
+// document.write("Division of "+num1+" and "+num2+" is "+div);
+// var mod=num1%num2;
+// document.write("Modulus of "+num1+" and "+num2+" is "+mod);
+//TASK 3:
+var x;
+document.write("Value after variable declaration is: "+x+"<br>");
+x=5;
+document.write("Initial value: "+x+"<br>");
+++x;
+document.write("Value after increment is: "+x+"<br>");
+x=x+7;
+document.write("Value after addition is: "+x+"<br>");
+--x;
+document.write("Value after decrement is: "+x+"<br>");
+x=x%3;
+document.write("The remainder is: "+x+"<br>");
+
+
