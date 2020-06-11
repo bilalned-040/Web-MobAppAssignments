a34cecc4301dd50f8d382c8e66445cbc58f9b415
Muhammad Bilal Hanif <bilalhanifhalai@gmail.com>
2020-06-12 00:57:20 +0500

    upto chap 9

--- Assignment 5/app.js
+++ Assignment 5/app.js
@@ -79,6 +79,8 @@
 
 
 
+
+
 //                                                  CHAPTER 5:
 //TASK 1:
 // var num1=parseInt(prompt("Enter 1st number:"));
@@ -95,17 +97,118 @@
 // var mod=num1%num2;
 // document.write("Modulus of "+num1+" and "+num2+" is "+mod);
 //TASK 3:
-var x;
-document.write("Value after variable declaration is: "+x+"<br>");
-x=5;
-document.write("Initial value: "+x+"<br>");
-++x;
-document.write("Value after increment is: "+x+"<br>");
-x=x+7;
-document.write("Value after addition is: "+x+"<br>");
---x;
-document.write("Value after decrement is: "+x+"<br>");
-x=x%3;
-document.write("The remainder is: "+x+"<br>");
+// var x;
+// document.write("Value after variable declaration is: "+x+"<br>");
+// x=5;
+// document.write("Initial value: "+x+"<br>");
+// ++x;
+// document.write("Value after increment is: "+x+"<br>");
+// x=x+7;
+// document.write("Value after addition is: "+x+"<br>");
+// --x;
+// document.write("Value after decrement is: "+x+"<br>");
+// x=x%3;
+// document.write("The remainder is: "+x+"<br>");
+//TASK 4:
+// var movieTic=600;
+// document.write("Total cost to buy 5 tickets to a movie is "+movieTic*5+"PKR");
+//TASK 5:
+// var tableNum=4;
+// document.write("Table of "+tableNum+"<br>");
+// for(var i =1;i<=10;i++){
+//     document.write(tableNum+"x"+i+"="+tableNum*i+"<br>");
+// }
+//TASK 6:
+// var cel=25;
+// var F=(cel*9/5)+32;
+// var fah=70;
+// var C=(fah-32)*5/9;
+// document.write(cel+"<sup>0</sup>C is "+F+"<sup>0</sup>F<br>");
+// document.write(fah+"<sup>0</sup>F is "+C+"<sup>0</sup>C");
+//TASK 7:
+// var item1=650;
+// var item2=100;
+// var qt1=3;
+// var qt2=7;
+// var shipCh=100;
+// var total=item1*qt1+item2*qt2+shipCh;
+// document.write("Price of item 1 is "+item1+"<br>");
+// document.write("Quantity of item 1 is "+qt1+"<br>");
+// document.write("Price of item 2 is "+item2+"<br>");
+// document.write("Quantity of item 2 is "+qt2+"<br>");
+// document.write("Shipping charges "+shipCh+"<br><br>");
+// document.write("Total cost of your order is is "+total+"<br>");
+//TASK 8:
+// var totalMarks=980;
+// var marksObt=804;
+// var per=marksObt/totalMarks*100;
+// document.write("Total marks: "+totalMarks+"<br>");
+// document.write("Marks obtained: "+marksObt+"<br>");
+// document.write("Percentage: "+per+"<br>");
+//TASK 9:
+// var usa=10;
+// var ksa=25;
+// var pkr=(usa*104.80)+(ksa*28);
+// document.write("<h2>Currency in PKR</h2><br>");
+// document.write("Total Currency in PKR: "+pkr);
+//TASK 10:
+// var x=10;
+// var result=(((x+5)*10)/2);
+// document.write(result);
+//TASK 11:
+// var cYear=2020;
+// var bYear=1999;
+// var age=cYear-bYear;
+// document.write("<h3>Age Calculator</h3><br>");
+// document.write("Cureent Year: "+cYear+"<br>");
+// document.write("Birth Year: "+bYear+"<br>");
+// document.write("Your Age is: "+age+"<br>");
+//TASK 12:
+// var r=20;
+// var circum=2*Math.PI*r;
+// var area=Math.PI*Math.pow(r,2);
+// document.write("<h2>The Geometrizer</h2><br>");
+// document.write("Radius of a circle: "+r+"<br>");
+// document.write("The circumference is: "+circum+"<br>");
+// document.write("The area is: "+area+"<br>");
+//TASK 13:
+// var fvrtSnack="Choclate Chip";
+// var cAge=15;
+// var eAge=65;
+// var snackPD=3;
+// var total=(eAge-cAge)*snackPD;
+// document.write("<h2>The Life Time Supply Calculator</h2><br>");
+// document.write("Favourite Snack: "+fvrtSnack+"<br>");
+// document.write("Current age: "+cAge+"<br>");
+// document.write("Estimated maximum age: "+eAge+"<br>");
+// document.write("Amount of snacks per day: "+snackPD+"<br>");
+// document.write("You will need "+total+" "+fvrtSnack+" to last you until the ripe old age of "+eAge);
+
+
+
+
+
+//                                                  CHAPTER 6-9:
+//TASK 1:
+// var a=10;
+// document.write("Result:<br>The value of a is: "+a+"<br><br>.................................<br><br>");
+// document.write("The value of ++a is: "+(++a)+"<br>Now the value of a is: "+a+"<br><br>");
+// document.write("The value of a++ is: "+(a++)+"<br>Now the value of a is: "+a+"<br><br>");
+// document.write("The value of --a is: "+(--a)+"<br>Now the value of a is: "+a+"<br><br>");
+// document.write("The value of a-- is: "+(a--)+"<br>Now the value of a is: "+a+"<br><br>");
+// TASK 2:
+// var a=2;
+// var b=1;
+// var result=--a - --b + ++b + b--;
+// document.write("--a = 1<br>--a - --b = 1 - 0<br>--a - --b + ++b = 1 - 0 + 1<br>--a - --b + ++b + b-- = 1 - 0 + 1 +1<br><br>");
+// document.write("a is: "+a+"<br>");
+// document.write("b is: "+b+"<br>");
+// document.write("Result is: "+result+"<br>");
+//TASK 3:
+
+
+
+
+
 
 
