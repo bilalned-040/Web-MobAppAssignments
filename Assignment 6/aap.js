8fd19089a04991b7ba52614d006ba95a45ac3e9b
Muhammad Bilal Hanif <bilalhanifhalai@gmail.com>
2020-06-20 02:01:19 +0500

    Assignment 6

---
+++ Assignment 6/aap.js
@@ -0,0 +1,57 @@
+//                                                  CHAPTER 21-25:
+//TASK 1:
+// var fName=prompt("Enter your first name:");
+// var lName=prompt("Enter your last name:");
+// var fullName=fName+" "+lName;
+// alert("Welcome "+fullName);
+//TASK 2:
+// var fvtPhone=prompt("Enter your favorite phone:");
+// document.write("My favotite phone is: "+fvtPhone+"<br>");
+// document.write("Length of string: "+fvtPhone.length);
+//TASK 3:
+// var str="Pakistani";
+// document.write("String: "+str+"<br>");
+// document.write("Index of 'n': "+str.indexOf("n"));
+//TASK 4:
+// var str="Hello World";
+// document.write("String: "+str+"<br>");
+// document.write("Last index of 'l': "+str.lastIndexOf("l"));
+//TASK 5:
+// var str="Pakistani";
+// document.write("String: "+str+"<br>");
+// document.write("Character at index 3: "+str.charAt(3));
+//TASK 6:
+// var fName=prompt("Enter your first name:");
+// var lName=prompt("Enter your last name:");
+// alert("Welcome "+fName.concat([" "],lName));
+//TASK 7:
+// var str1="Hyderabad";
+// var str2=str1.replace("Hyder","Islam");
+// document.write("City: "+str1+"<br>");
+// document.write("After replacement: "+str2);
+//TASK 8:
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var rep=message.replace(/and/g,"&");
+// document.write("Message: "+"<br>"+message+"<br><br>");
+// document.write("After replacement: "+"<br>"+rep);
+//TASK 9:
+// var str="472";
+// var num=Number(str);
+// document.write("Value: "+str+"<br>"+"Type: "+typeof(str)+"<br>");
+// document.write("Value: "+num+"<br>"+"Type: "+typeof(num));
+//TASK 10:
+// var input=prompt("Enter a string:");
+// document.write("User input: "+input+"<br>");
+// document.write("Upper case: "+input.toUpperCase());
+//TASK 11:
+// var str=prompt("Enter a string");
+// var firstChar=str.slice(0,1).toUpperCase();
+// var otherChar=str.slice(1).toLowerCase();
+// var titledStr=firstChar+otherChar;
+// document.write("User input: "+str+"<br>");
+// document.write("Title case: "+titledStr);
+//TASK 12:
+// var num=35.36;
+// var str=num.toString().replace(".","");
+// document.write("Number: "+num+"<br>")
+// document.write("Result: "+str);
